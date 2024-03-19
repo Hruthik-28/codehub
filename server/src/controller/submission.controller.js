2d0046bd8de16a414ca91136b23a40a5044ab6f0
HRUTHIK <hruthik9980819630@gmail.com>
2024-03-19 13:57:02 +0530

    implement Generation of token for source_code using Judge0 and getting its stdout in submission controller
    - changed queries in both controllers
    - changed schema of sql database

--- server/src/controller/submission.controller.js
+++ server/src/controller/submission.controller.js
@@ -5,6 +5,22 @@ import axios from "axios";
 import { pool } from "../index.js";
 import axiosInstance from "../utils/axiosInstance.js";
 
+function getLanguageId(code_language) {
+    const languageIds = {
+        "C++": 52,
+        "Java": 62,
+        "JavaScript": 63,
+        "Python": 71,
+    };
+
+    if (languageIds.hasOwnProperty(code_language)) {
+        return languageIds[code_language];
+    } else {
+        console.error(`Language ID not found for ${code_language}`);
+        return null;
+    }
+}
+
 export const submission = asyncHanlder(async (req, res) => {
     const { username, code_language, stdin, source_code } = req.body;
 
@@ -17,30 +33,31 @@ export const submission = asyncHanlder(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    // Get language_id from languages table for respective language
-    const [row] = await pool.query(
-        "SELECT lang_id FROM languages WHERE lang_name = ?",
-        [code_language?.toLowerCase()]
-    );
-    if (row.length === 0) {
-        throw new ApiError(400, "Invalid code language");
-    }
+    // get language_id
+    const language_id = getLanguageId(code_language);
 
-    // extract language_id
-    const language_id = row[0].lang_id;
-
-    //Create a submission and get the token for source_code from Judge0
-    const response = await axiosInstance.post("/submissions", {
+    const data = {
         language_id,
         source_code,
         stdin
-    });
-    const token = response?.token;
+    };
+    // Generate a token for source_code using Judge0
+    const response_one = await axiosInstance.post("/submissions", data);
+    const token = response_one?.data?.token;
 
-    // // save all the details with token in the DB
+    if (!token) {
+        throw new ApiError(500, "Failed to generate token");
+    }
+
+    let stdout;
+    // extract stdout using token from Judge0
+    const response_two = await axiosInstance.get(`/submissions/${token}`);
+    stdout = response_two?.data.stdout;
+
+    // save all the details with stdin and stdout in the DB
     const result = await pool.query(
-        "INSERT INTO submissions (username, language_id, stdin, source_code, token) VALUES (?, ?, ?, ?)",
-        [username, language_id, stdin, source_code, token]
+        "INSERT INTO submissions (username, code_language, stdin, stdout, source_code) VALUES (?, ?, ?, ?, ?)",
+        [username, code_language, stdin, stdout, source_code]
     );
     if (result?.length === 0) {
         throw new ApiError(500, "Error executing SQL query");
@@ -52,27 +69,29 @@ export const submission = asyncHanlder(async (req, res) => {
 });
 
 export const getSubmissions = asyncHanlder(async (req, res) => {
-    pool.query("SELECT * FROM submissions", (err, results) => {
-        if (err) {
-            console.error("Error executing SQL query:", err);
-            throw new ApiError(
-                500,
-                "An error occurred while processing your request"
-            );
-        }
+    const [rows] = await pool.query("SELECT * FROM submissions");
 
-        // iterate thruough results.source_code to limit it to 100 chars
-        const limitedResult = results?.map((result) => ({
-            ...result,
-            source_code: result.source_code.substring(0, 3)
-        }));
+    if (rows.length === 0) {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "No Submissions available"));
+    }
 
-        res.status(200).json(
+    // iterate thruough rows to limit rows.source_code to 100 chars
+    const limitedResult = rows?.map((row) => {
+        return {
+            ...row,
+            source_code: row.source_code.substring(0, 100)
+        };
+    });
+
+    return res
+        .status(200)
+        .json(
             new ApiResponse(
                 200,
                 limitedResult,
                 "Submissions fetched successfully"
             )
         );
-    });
 });
--- server/src/utils/axiosInstance.js
+++ server/src/utils/axiosInstance.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
-
-axios.defaults.baseURL = "https://judge0-ce.p.rapidapi.com";
-axios.defaults.params = {
-    base64_encoded: "true",
-    fields: "*"
-};
-axios.defaults.headers = {
-    "X-RapidAPI-Key": process.env.X_RAPID_API_KEY,
-    "X-RapidAPI-Host": process.env.X_RAPID_API_HOST
-};
+const axiosInstance = axios.create({
+    baseURL: "https://judge0-ce.p.rapidapi.com",
+    headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": process.env.X_RAPID_API_KEY,
+        "X-RapidAPI-Host": process.env.X_RAPID_API_HOST
+    },
+    params: {
+        base64_encoded: "true",
+        fields: "stdout"
+    }
+});
 
 export default axiosInstance;
