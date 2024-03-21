70d54cd4f83d065754a43e9ca4318bff1475eefa
HRUTHIK <hruthik9980819630@gmail.com>
2024-03-21 14:33:14 +0530

    set credentials true in axiosInstance

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-axiosInstance.defaults.withCredentials = false;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
